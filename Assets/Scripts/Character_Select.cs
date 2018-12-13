bb812f42d23c956ec11554a0aef708585e802838
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2018-12-13 17:35:57 +0000

    Got the character Selection to work

---
+++ Assets/Scripts/Character_Select.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+public class Character_Select : MonoBehaviour {
+
+    public static int PlayerNum;
+
+
+	public void CharacterSelect (int selectedNum) {
+
+        PlayerNum = selectedNum;
+
+        Application.LoadLevel("Level_1");
+	}
+}
---
+++ Assets/Scripts/Character_Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Spawner : MonoBehaviour {
+
+    //Creates a list of game objects that can be accessed
+    public GameObject[] Characters;
+
+    //creates a variable for the variable
+    public Transform PlayerSpawnPoint;
+
+    
+    void Start () {
+
+        //pulls the object from thenumber on the list and spawns it at the place and rotation that is set in the other variables.
+        Instantiate(Characters[Character_Select.PlayerNum], PlayerSpawnPoint.position, PlayerSpawnPoint.rotation);
+	}
+	
+}
