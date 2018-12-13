ff2d85cecd4044c8e4cba0b7c6e2632ece7c117c
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2018-12-13 16:25:03 +0000

    I have managed to get the select a character to work for one person

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Character")]
+public class Character : ScriptableObject  {
+
+    public string characterName = "Default";
+    public int startingHp = 100; 
+}
--- Assets/Scripts/CharacterSelector.cs
+++ Assets/Scripts/CharacterSelector.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CharacterSelector : MonoBehaviour {
+public class CharacterSelector : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject player;
+    public Vector3 playerSpawnPosition = new Vector3(-9, -1, 0);
+
+    public Character[] characters;
+
+    public GameObject characterSelectPanel;
+    
+
+    //Function that will be called when the button is pressed
+    //will set the variable "StartGame" to an integer
+    public void StartGame(int characterChoice)
+    {
+        characterSelectPanel.SetActive(false);
+
+        //declares game object called spawn player which is equal to the playe, the players spawn coordinates and the rotation of the prefab 
+        //also casts to a game object
+        //GameObject spawnedPlayer = Instantiate(player, playerSpawnPosition, Quaternion.identity) as GameObject;
+        //Character selectedCharacter = characters [characterChoice];
+
+        SceneManager.LoadScene("Level_1");
+    }
+
+
+    
+
+
+
+
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
