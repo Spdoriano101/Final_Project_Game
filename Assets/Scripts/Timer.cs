cde24996506fbfaf0464f77f61b41d41e47754e4
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2018-12-18 23:48:14 +0000

    almost have the lives working

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    float timeLeft = 30.0f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene("Game_Over");
+        }
+    }
+}
