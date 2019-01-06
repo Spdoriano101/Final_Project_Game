681d14b0a713470ee6e807166564eb4039b2ad0e
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2019-01-06 19:32:05 +0000

    created the title menu so can return from the game over screen

---
+++ Assets/Scripts/Button Scripts/Title_Screen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_Screen : MonoBehaviour {
+
+	// Use this for initialization
+	public void startGame () {
+
+        SceneManager.LoadScene("Start_Menu");
+
+    }
+	
+	
+}
